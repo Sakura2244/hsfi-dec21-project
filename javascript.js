a9f3c6a95d423b9abc2df6f289bd365fe9153a32
Jennifer <jenniferzheng23@gmail.com>
2017-01-04 22:10:21 +0000

    add new buttons

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@
 This function is fired when the webpage is loaded
 */
 $(document).ready(function(){
-   $('.all-content').hide(); 
+   $(".all-content").hide(); 
 });
 
 /*
@@ -10,8 +10,8 @@ When someone clicks goals-button we should hide all-content
 and then show only goals-content
 */
 $("#goals-button").click(function(){
-    $('.all-content').hide();
-    $('#goals-content').show();
+    $(".all-content").hide();
+    $("#goals-content").show();
 });
 
 
@@ -20,6 +20,31 @@ When someone clicks the info button we should hide all-content
 and then show only info-content
 */
 $("#info-button").click(function(){
-    $('.all-content').hide();
-    $('#info-content').show();
+    $(".all-content").hide();
+    $("#info-content").show();
 });
+$("#past-button").click(function(){
+    $(".all-content").hide();
+    $("#past-content").show();
+});
+
+
+$("#dark-button").click(function(){
+    $("body").css("background-color","black");
+   $(".all-content").css("color","white");
+});
+$("#donotpress-button").click(function(){
+    alert("DO NOT PRESS");
+});
+$("#take-over-button").click(function(){
+ $(".all-content").hide();
+    $("#takeover-content").show();
+});
+$("#takeover-form-button").click(function(){
+    var name=$("#takeover-input").val();
+    $("#site-title").html("This website belongs to:" + name);
+    $("#takeover-input").val("");
+    $("#site-title").css("background-color","#800020");
+   $("#site-title").css("color","white");
+});
+    
